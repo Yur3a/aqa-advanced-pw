28604ee9a0b3fecbf3af2c41d10bfba71f9a0ebd
Yurii Zatochnyi <yurii.zatochnyi@neuraspace.com>
2024-11-09 18:46:54 +0200

    changed generateRandomEmail

--- utils/randomEmailGenerator.ts
+++ utils/randomEmailGenerator.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default function generateRandomEmail(): string {
-    const emailPrefix = 'michael.krasnovskyi+aqa-user';
+    const emailPrefix = 'yurii_z+aqa-user';
     const domain = 'gmail.com';
     const uuid = uuidv4().substr(0, 8);
     return `${emailPrefix}${uuid}@${domain}`;
